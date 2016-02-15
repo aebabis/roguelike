9d6cd230125a752bbe71aab62122e0cf6e7259c3
Alex Babis <acbabis@gmail.com>
2016-02-15 13:48:25 -0700

    es6Inject for unit testing

--- test/Creature-test.js
+++ test/Creature-test.js
@@ -1,54 +1,40 @@
 var System = require('es6-module-loader').System;
 var expect = require('chai').expect;
 
-var depList = [
-    'js/app/dungeons/Dungeon.js',
-    'js/app/dungeons/TestDungeonFactory.js',
-    'js/app/entities/creatures/Creature.js',
-    'js/app/entities/creatures/PlayableCharacter.js',
-    'js/app/tiles/WallTile.js'
-];
-
-var MODULES = {};
-
 describe('Creature', function() {
-    before(function(done) {
-        // Files have to be loaded sequentially to prevent traceur/es6-module-loader
-        // bug.
-        depList.reduce(function(chain, fileName) {
-            return chain.then(function() {
-                return System.import(fileName).then(function(module) {
-                    var modName = fileName.split(/[\/\.]/);
-                    modName = modName[modName.length - 2];
-                    MODULES[modName] = module.default;
-                });
-            });
-        }, Promise.resolve()).then(done, done);
+    beforeEach(function(done) {
+        es6Inject([
+            'js/app/dungeons/Dungeon.js',
+            'js/app/dungeons/TestDungeonFactory.js',
+            'js/app/entities/creatures/Creature.js',
+            'js/app/entities/creatures/PlayableCharacter.js',
+            'js/app/tiles/WallTile.js'
+        ]).then(done, done);
     });
 
     it('should be able to see an adjacent tile', function() {
-        var dungeon = new MODULES['TestDungeonFactory']().getEmptyDungeon();
+        var dungeon = new TestDungeonFactory().getEmptyDungeon();
         var creature = dungeon.getPlayableCharacter();
 
         expect(creature.canSee(dungeon.getTile(0, 1))).to.equal(true);
     });
 
     it('should be able to see in a straight line', function() {
-        var dungeon = new MODULES['TestDungeonFactory']().getEmptyDungeon();
+        var dungeon = new TestDungeonFactory().getEmptyDungeon();
         var creature = dungeon.getPlayableCharacter();
 
         expect(creature.canSee(dungeon.getTile(4, 1))).to.equal(true);
     });
 
     it('should not see through walls', function() {
-        var dungeon = new MODULES['TestDungeonFactory']().getLineDungeon();
+        var dungeon = new TestDungeonFactory().getLineDungeon();
         var creature = dungeon.getPlayableCharacter();
 
         expect(creature.canSee(dungeon.getTile(0, 0))).to.equal(false);
     });
 
     it('should be able to see across corners', function() {
-        var dungeon = new MODULES['TestDungeonFactory']().getODungeon();
+        var dungeon = new TestDungeonFactory().getODungeon();
         var creature = dungeon.getPlayableCharacter();
 
         expect(creature.canSee(dungeon.getTile(0, 0))).to.equal(true);

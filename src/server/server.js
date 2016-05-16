08be3941433832eb38c598cd61433161fb50c29b
Alex Babis <acbabis@gmail.com>
2016-05-15 20:17:24 -0700

    Using process.env.PORT

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var app = express();
 
-var argv = require('yargs')
-    .alias('p', 'port')
-    .argv;
-
-    console.log(argv);
-
 app.use("/bower_components", express.static(__dirname + "/../../bower_components"));
 app.use("/node_modules", express.static(__dirname + "/../../node_modules"));
 
 app.use("/", express.static(__dirname + "/../client"));
 
 
-app.listen(argv.port || 80);
+app.listen(process.env.PORT || 8080);

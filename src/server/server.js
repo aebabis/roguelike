9ae2792e2ee7c9d3ef27b645f5da87d614f841ca
Alex Babis <acbabis@gmail.com>
2016-05-29 12:44:56 -0700

    Server works

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use("/bower_components", express.static(__dirname + "/../../bower_components"));
 app.use("/node_modules", express.static(__dirname + "/../../node_modules"));
 
-app.use("/", express.static(__dirname + "/../client"));
+app.use("/", express.static(__dirname + "/../../dist"));
 
 
 app.listen(process.env.PORT || 8080);

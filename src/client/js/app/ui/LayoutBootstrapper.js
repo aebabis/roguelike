0e0e4e7ea931614f074c529539c97165c07f621e
Alex Babis <acbabis@gmail.com>
2016-05-28 20:24:08 -0700

    CSS bundling works

--- src/client/js/app/ui/LayoutBootstrapper.js
+++ src/client/js/app/ui/LayoutBootstrapper.js
@@ -1,3 +1,9 @@
+// http://stackoverflow.com/a/31770875/2993478
+var req = require.context("../../../css", true, /^(.*\.(css$))[^.]*$/igm);
+req.keys().forEach(function(key){
+    req(key);
+});
+
 import { default as MenuBar } from "./MenuBar.js";
 import { default as TestBootstrapper } from '../TestBootstrapper.js';
 import { default as Classes } from '../entities/creatures/classes/Classes.js';
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,6 @@ module.exports = {
     module: {
         loaders: [
             {
-                loader: "style!css",
-                test: /\.css$/
-            }, {
                 loader: 'babel-loader',
                 //include: [path.resolve(__dirname, 'src')],
 
@@ -19,7 +16,22 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            }, {
+                //loader: "style!css",
+                loader: "style-loader!css-loader",
+                test: /\.css$/
+            }, {
+                loader: "url-loader?limit=100000",
+                test: /\.png$/
             }
+        ]
+    },
+
+    // http://stackoverflow.com/questions/35171288/error-cannot-resolve-module-style-loader
+    resolve: {
+        extensions: ['', '.js', '.css'],
+        modulesDirectories: [
+          'node_modules'
         ]
     }
 };

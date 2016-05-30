f6426a36f878bb0e8dc68debeb2bc425fd717992
Alex Babis <acbabis@gmail.com>
2016-05-29 18:41:07 -0700

    Adding GZIP

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var compression = require('compression');
+app.use(compression());
 
 app.use("/bower_components", express.static(__dirname + "/../../bower_components"));
 app.use("/node_modules", express.static(__dirname + "/../../node_modules"));

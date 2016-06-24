d8877bd01fce91b43a6f215d7a3f2848ae84d07b
Alex Babis <acbabis@gmail.com>
2016-06-24 00:53:23 -0700

    Preventing illegal moves in mouse controller

--- src/client/js/app/controllers/GraphicalViewMouseController.js
+++ src/client/js/app/controllers/GraphicalViewMouseController.js
@@ -4,35 +4,48 @@ export default class GraphicalViewMouseController {
     constructor(dungeon, sharedData, graphicalDungeonView) {
         var dom = graphicalDungeonView.getDom();
 
-        // Arrow key handler
-        $(dom).on('click', '.cell', function(event) {
-            var tile = this;
+        function getMoveFor(tileDom) {
             var dungeon = sharedData.getDungeon();
-            var character = dungeon.getPlayableCharacter();
-            var playerLocation = dungeon.getTile(character);
-            var targetX = tile.getAttribute('data-x');
-            var targetY = tile.getAttribute('data-y');
+            var player = dungeon.getPlayableCharacter();
+            var playerLocation = dungeon.getTile(player);
+            var targetX = tileDom.getAttribute('data-x');
+            var targetY = tileDom.getAttribute('data-y');
             var creature = dungeon.getTile(targetX, targetY).getCreature();
             var abilityIndex = sharedData.getTargettedAbility();
             var itemIndex = sharedData.getTargettedItem();
             if(abilityIndex !== null) {
-                character.setNextMove(new Move.UseAbilityMove(playerLocation, abilityIndex, targetX, targetY));
                 sharedData.unsetTargettedAbility();
+                return new Move.UseAbilityMove(playerLocation, abilityIndex, targetX, targetY);
             } else if(itemIndex !== null) {
-                character.setNextMove(new Move.UseItemMove(playerLocation, itemIndex, dungeon.getTile(targetX, targetY)));
                 sharedData.unsetTargettedItem();
-            } else if(creature && creature.isEnemy(character)) {
-                character.setNextMove(new Move.AttackMove(playerLocation, targetX, targetY));
+                return new Move.UseItemMove(playerLocation, itemIndex, dungeon.getTile(targetX, targetY));
+            } else if(creature && creature.isEnemy(player)) {
+                return new Move.AttackMove(playerLocation, targetX, targetY);
             } else {
                 var dx = targetX - playerLocation.getX();
                 var dy = targetY - playerLocation.getY();
                 if(dx === 0 && dy === 0) {
-                    character.setNextMove(new Move.WaitMove(playerLocation));
+                    return new Move.WaitMove(playerLocation);
                 } else if(Math.abs(dx) <= 1 && Math.abs(dy) <= 1 && (dx !== 0 || dy !== 0)) {
-                    character.setNextMove(new Move.MovementMove(playerLocation, dx, dy));
+                    return new Move.MovementMove(playerLocation, dx, dy);
                 }
             }
-            dungeon.resolveUntilBlocked();
+        }
+
+        // Arrow key handler
+        $(dom).on('click', '.cell', function(event) {
+            var move = getMoveFor(this);
+            if(move) {
+                var dungeon = sharedData.getDungeon();
+                var player = dungeon.getPlayableCharacter();
+                var reason = move.getReasonIllegal(dungeon, player);
+                if(reason) {
+                    console.warn(reason);
+                } else {
+                    player.setNextMove(move);
+                    dungeon.resolveUntilBlocked();
+                }
+            }
         });
 
         $(dom).on('mouseover', '.cell', function() {

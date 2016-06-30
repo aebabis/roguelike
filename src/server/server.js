a5594e4741de5d00c483f2fb3c1b577ddc14c73e
Alex Babis <acbabis@gmail.com>
2016-06-30 00:14:46 -0700

    Linux line endings

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,9 @@ var app = express();
 var compression = require('compression');
 app.use(compression());
 
-app.use("/bower_components", express.static(__dirname + "/../../bower_components"));
-app.use("/node_modules", express.static(__dirname + "/../../node_modules"));
+app.use('/bower_components', express.static(__dirname + '/../../bower_components'));
+app.use('/node_modules', express.static(__dirname + '/../../node_modules'));
 
-app.use("/", express.static(__dirname + "/../../dist"));
-
+app.use('/', express.static(__dirname + '/../../dist'));
 
 app.listen(process.env.PORT || 8080);

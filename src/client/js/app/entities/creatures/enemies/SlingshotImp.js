b9a8963d2fb5c6f5495d416e8fcd2b543faf1ac8
Alex Babis <acbabis@gmail.com>
2016-07-01 00:35:47 -0700

    Made Imp use same sling as player

--- src/client/js/app/entities/creatures/enemies/SlingshotImp.js
+++ src/client/js/app/entities/creatures/enemies/SlingshotImp.js
@@ -1,43 +1,21 @@
 import Creature from '../Creature.js';
 import Strategies from '../strategies/Strategies.js';
 
-import Weapon from '../../weapons/Weapon.js';
-import DamageTypes from '../../DamageTypes.js';
-
-class ImpSlingshot extends Weapon {
-    getRange() {
-        return 3;
-    }
-
-    getDamage() {
-        return 2;
-    }
-
-    getDamageType() {
-        return DamageTypes.RANGED_PHYSICAL;
-    }
-}
+import Sling from '../../weapons/Sling.js';
 
 export default class SlingshotImp extends Creature {
     /**
       * @class SlingshotImp
-      * @description Basic ranged enemy. Stands in place an shoots enemies
+      * @description Basic ranged enemy.
       */
     constructor() {
         super();
+        this.addItem(new Sling());
         this.setStrategy(new Strategies.CompositeStrategy(
             new Strategies.ChaseStrategy(),
             new Strategies.FleeStrategy(),
             new Strategies.IdleStrategy()
         ));
-    }
-
-    getMeleeWeapon() {
-        return null;
-    }
-
-    getRangedWeapon() {
-        return new ImpSlingshot();
     }
 
     getBaseHP() {

1150d1196c00b5456f755801dfe9a6254b5ac5a8
Alex Babis <acbabis@gmail.com>
2016-12-17 13:38:19 -0700

    Prod and dev use same entry point. This will make it easier to develop a backend

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,6 @@ var app = express();
 var compression = require('compression');
 app.use(compression());
 
-app.use('/bower_components', express.static(__dirname + '/../../bower_components'));
-app.use('/node_modules', express.static(__dirname + '/../../node_modules'));
-
-app.use('/', express.static(__dirname + '/../../dist'));
+require('./serve-static')(app);
 
 app.listen(process.env.PORT || 8080);

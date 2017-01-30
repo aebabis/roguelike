bbc6489dd642c743ec5e33add0545667b087e7b5
Alex Babis <acbabis@gmail.com>
2017-01-29 22:00:47 -0700

    More refactoring

--- src/client/js/app/views/PixiDungeonView.js
+++ src/client/js/app/views/PixiDungeonView.js
@@ -124,6 +124,25 @@ function getIndicator(x, y, color) {
     return indicator;
 }
 
+function getTileContainer(tile) {
+    const tileContainer = new PIXI.Container(); // TODO: Look at ParticleContainer
+    const tileSprite = getTileSprite(tile);
+    const x = tile.getX();
+    const y = tile.getY();
+
+    tileSprite.x = 0;
+    tileSprite.y = 0;
+    tileSprite.width = TILE_WIDTH;
+    tileSprite.height = TILE_WIDTH;
+    tileSprite.interactive = true;
+
+    tileContainer.addChild(tileSprite);
+    tileContainer.addChild(new PIXI.Container());
+    tileContainer.addChild(new PIXI.Container());
+
+    return tileContainer;
+}
+
 export default class PixiDungeonView {
     constructor(sharedData) {
         if(!(sharedData instanceof SharedUIDataController)) {
@@ -154,92 +173,10 @@ export default class PixiDungeonView {
 
     init() {
         const self = this;
-        const stage = new PIXI.Container();
+        const stage = this._stage = new PIXI.Container();
         stage.backgroundColor = 'gray';
 
-        let tileContainers;
-        const entitySprites = {};
-
-        function getTileContainer(tile) {
-            const tileContainer = new PIXI.Container(); // TODO: Look at ParticleContainer
-            const tileSprite = getTileSprite(tile);
-            const x = tile.getX();
-            const y = tile.getY();
-
-            tileSprite.x = 0;
-            tileSprite.y = 0;
-            tileSprite.width = TILE_WIDTH;
-            tileSprite.height = TILE_WIDTH;
-            tileSprite.interactive = true;
-
-            tileSprite.on('click', function(event) {
-                self._clickHanders.forEach(function(handler) {
-                    handler(x, y);
-                });
-            }).on('mouseover', function(event) {
-                setTimeout(function() { // Ensure mouseout fires first
-                    self._mouseOverHandlers.forEach(function(handler) {
-                        handler(x, y);
-                    });
-                });
-            }).on('mouseout', function(event) {
-                self._mouseOutHandlers.forEach(function(handler) {
-                    handler(x, y);
-                });
-            });
-
-            tileContainer.addChild(tileSprite);
-            tileContainer.addChild(new PIXI.Container());
-            tileContainer.addChild(new PIXI.Container());
-
-            return tileContainer;
-        }
-
-        function populateSprites() {
-            while(stage.children.length) stage.removeChild(stage.children[0]);
-
-            const dungeon = sharedData.getDungeon();
-            tileContainers = new Array(dungeon.getWidth()).fill(0).map(()=>[]);
-            
-            dungeon.forEachTile(function(tile, x, y) {
-                const tileContainer = getTileContainer(tile);
-                tileContainer.x = x * (TILE_WIDTH + GAP_WIDTH);
-                tileContainer.y = y * (TILE_WIDTH + GAP_WIDTH);
-                tileContainers[x][y] = tileContainer;
-
-                const creature = tile.getCreature();
-                if(creature) {
-                    const creatureSprite = setDefaultSpriteProps(getCreatureSprite(creature));
-                    entitySprites[creature.getId()] = creatureSprite;
-                    tileContainer.children[2].addChild(creatureSprite);
-                }
-
-                stage.addChild(tileContainer);
-            });
-
-            updateVision();
-        }
-
-        function updateVision() {
-            const dungeon = sharedData.getDungeon();
-            const player = dungeon.getPlayableCharacter();
-            if(player) {
-                dungeon.forEachTile(function(tile, x, y) {
-                    const creature = tile.getCreature();
-                    const tileContainer = tileContainers[x][y];
-                    const [tileSprite, itemContainer, creatureContainer] = tileContainer.children;
-                    const playerCanSeeTile = player.canSee(dungeon, tile);
-                    if(playerCanSeeTile) {
-                        tileContainer.alpha = 1;
-                    } else if(player.hasSeen(tile)) {
-                        tileContainer.alpha = .5;
-                    } else {
-                        tileContainer.alpha = 0;
-                    }
-                    itemContainer.visible = creatureContainer.visible = playerCanSeeTile;
-                });
-            }
-        }
+        const entitySprites = this._entiteSprites = {};
 
         let rangeIndicator;
         function updateRangeIndicator() {
@@ -308,96 +245,164 @@ export default class PixiDungeonView {
             });
         }
 
-        function updateCreatureLocations() {
-            const dungeon = sharedData.getDungeon();
-            dungeon.getCreatures().forEach(function(creature) {
-                const sprite = entitySprites[creature.getId()] || 
-                        (entitySprites[creature.getId()] = setDefaultSpriteProps(getCreatureSprite(creature)));
-                if(sprite.parent) {
-                    sprite.parent.removeChild(sprite);
-                }
-                if(!creature.isDead()) {
-                    const tile = dungeon.getTile(creature);
-                    tileContainers[tile.getX()][tile.getY()].children[2].addChild(sprite);
-                }
-            })
-        }
-
-        function updateItems() {
-            const dungeon = sharedData.getDungeon();
-            dungeon.forEachTile(function(tile, x, y) {
-                const tileContainer = tileContainers[x][y];
-                const itemsContainer = tileContainer.children[1];
-                while(itemsContainer.children.length) itemsContainer.removeChildAt(0);
-                tile.getItems().forEach(function(item) {
-                    itemsContainer.addChild(setDefaultSpriteProps(new PIXI.Sprite(PIXI.utils.TextureCache[item.constructor.name])));
-                })
-            });
-        }
-
-        function scroll() {
-            const dungeon = sharedData.getDungeon();
-            const player = dungeon.getPlayableCharacter();
-            const tile = dungeon.getTile(player);
-
-            const canvasWidth = self.getDom().firstChild.width;
-            const canvasHeight = self.getDom().firstChild.height;
-
-            const cellDimension = TILE_WIDTH;
-            const halfDimension = TILE_WIDTH / 2;
-            const playerOffsetX = tile.getX() * cellDimension + halfDimension;
-            const playerOffsetY = tile.getY() * cellDimension + halfDimension;
-
-            stage.x = (canvasWidth / 2) - playerOffsetX;
-            stage.y = (canvasHeight / 2) - playerOffsetY;
-        }
-
-        function populateStage() {
-            populateSprites();
-            updateItems();
-            updateVision();
-            renderer.render(stage);
-        }
-
         const sharedData = this._sharedData;
         const renderer = this._renderer;
         sharedData.addObserver(function observer(event) {
             if(event instanceof Dungeon){
-                populateStage();
+                self.populateStage(stage);
                 document.querySelector('section.game').focus(); //TODO: Make canvas container focusable insted?
             }
             updateSelectedTileIndicator();
         });
 
-        sharedData.addObserver(function observer(event) {
+        sharedData.addObserver((event) => {
             const dungeon = sharedData.getDungeon();
             if(event instanceof GameEvents.MoveEvent || event instanceof GameEvents.PositionChangeEvent || event instanceof GameEvents.SpawnEvent) {
-                updateVision();
-                updateCreatureLocations();
-                renderer.render(stage);
+                this.updateVision();
+                this.updateCreatureLocations();
+                this.scrollToPlayer();
             } else if(event instanceof GameEvents.DeathEvent) {
                 const creature = event.getCreature();
                 const tile = dungeon.getTile(creature);
                 const x = tile.getX();
                 const y = tile.getY();
-                const tileContainer = tileContainers[x][y].children[2].removeChildAt(0);
-                setTimeout(updateItems); // TODO: Fix with item spawn/drop event
+                const tileContainer = this._tileContainers[x][y].children[2].removeChildAt(0);
+                //setTimeout(updateItems); // TODO: Fix with item spawn/drop event
             } else if(event instanceof GameEvents.HitpointsEvent) {
                 /*if(event.getAmount() < 0) {
                     getScrollingText(event.getAmount(), x, y, DAMAGE_COLORS[event.getDamageType()] || 'green', DAMAGE_OUTLINE_COLORS[event.getDamageType()] || 'green')
                         .appendTo(grid.children[0]);
                 }*/
             } else if(event instanceof GameEvents.TakeItemEvent) {
-                updateItems();
+                this.updateItems();
             }
-            scroll();
+            this.scrollToPlayer();
             updateRangeIndicator();
             renderer.render(stage);
         });
 
-        scroll();
         updateRangeIndicator();
         renderer.render(stage);
+    }
+
+    populateStage(stage) {
+        this.populateSprites(stage);
+        this.updateCreatureLocations();
+        this.updateItems();
+        this.updateVision();
+        this._renderer.render(stage);
+    }
+    
+    populateSprites(stage) {
+        const self = this;
+        while(stage.children.length) stage.removeChild(stage.children[0]);
+
+        const dungeon = this._sharedData.getDungeon();
+        const tileContainers = this._tileContainers = new Array(dungeon.getWidth()).fill(0).map(()=>[]);
+        const entitySprites = this._entiteSprites;
+        
+        dungeon.forEachTile(function(tile, x, y) {
+            const tileContainer = getTileContainer(tile);
+            tileContainer.x = x * (TILE_WIDTH + GAP_WIDTH);
+            tileContainer.y = y * (TILE_WIDTH + GAP_WIDTH);
+            tileContainers[x][y] = tileContainer;
+
+            tileContainer.children[0].on('click', function(event) {
+                self._clickHanders.forEach(function(handler) {
+                    handler(x, y);
+                });
+            }).on('mouseover', function(event) {
+                setTimeout(function() { // Ensure mouseout fires first
+                    self._mouseOverHandlers.forEach(function(handler) {
+                        handler(x, y);
+                    });
+                });
+            }).on('mouseout', function(event) {
+                self._mouseOutHandlers.forEach(function(handler) {
+                    handler(x, y);
+                });
+            });
+
+            /*const creature = tile.getCreature();
+            if(creature) {
+                const creatureSprite = setDefaultSpriteProps(getCreatureSprite(creature));
+                entitySprites[creature.getId()] = creatureSprite;
+                tileContainer.children[2].addChild(creatureSprite);
+            }*/
+
+            stage.addChild(tileContainer);
+        });
+    }
+
+    updateItems() {
+        const dungeon = this._sharedData.getDungeon();
+        const tileContainers = this._tileContainers;
+        dungeon.forEachTile(function(tile, x, y) {
+            const tileContainer = tileContainers[x][y];
+            const itemsContainer = tileContainer.children[1];
+            while(itemsContainer.children.length) itemsContainer.removeChildAt(0);
+            tile.getItems().forEach(function(item) {
+                itemsContainer.addChild(setDefaultSpriteProps(new PIXI.Sprite(PIXI.utils.TextureCache[item.constructor.name])));
+            })
+        });
+    }
+
+    updateVision() {
+        const dungeon = this._sharedData.getDungeon();
+        const player = dungeon.getPlayableCharacter();
+        const tileContainers = this._tileContainers;
+        if(player) {
+            dungeon.forEachTile(function(tile, x, y) {
+                const creature = tile.getCreature();
+                const tileContainer = tileContainers[x][y];
+                const [tileSprite, itemContainer, creatureContainer] = tileContainer.children;
+                const playerCanSeeTile = player.canSee(dungeon, tile);
+                if(playerCanSeeTile) {
+                    tileContainer.alpha = 1;
+                } else if(player.hasSeen(tile)) {
+                    tileContainer.alpha = .5;
+                } else {
+                    tileContainer.alpha = 0;
+                }
+                itemContainer.visible = creatureContainer.visible = playerCanSeeTile;
+            });
+        }
+    }
+
+    updateCreatureLocations() {
+        const dungeon = this._sharedData.getDungeon();
+        const entitySprites = this._entiteSprites; // TODO: Getter
+        const tileContainers = this._tileContainers;
+        dungeon.getCreatures().forEach(function(creature) {
+            const sprite = entitySprites[creature.getId()] || 
+                    (entitySprites[creature.getId()] = setDefaultSpriteProps(getCreatureSprite(creature)));
+            if(sprite.parent) {
+                sprite.parent.removeChild(sprite);
+            }
+            if(!creature.isDead()) {
+                const tile = dungeon.getTile(creature);
+                tileContainers[tile.getX()][tile.getY()].children[2].addChild(sprite);
+            }
+        })
+    }
+
+    scrollToPlayer() {
+        const sharedData = this._sharedData;
+        const stage = this._stage;
+        const dungeon = sharedData.getDungeon();
+        const player = dungeon.getPlayableCharacter();
+        const tile = dungeon.getTile(player);
+
+        const canvasWidth = this.getDom().firstChild.width;
+        const canvasHeight = this.getDom().firstChild.height;
+
+        const cellDimension = TILE_WIDTH;
+        const halfDimension = TILE_WIDTH / 2;
+        const playerOffsetX = tile.getX() * cellDimension + halfDimension;
+        const playerOffsetY = tile.getY() * cellDimension + halfDimension;
+
+        stage.x = (canvasWidth / 2) - playerOffsetX;
+        stage.y = (canvasHeight / 2) - playerOffsetY;
     }
 
     getDom() {

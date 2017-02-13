2486d4154489f8606f1e3496af3918537bf6b6df
Alex Babis <acbabis@gmail.com>
2017-02-13 00:42:30 -0700

    Refactoring unit test

--- test/client/entities/creatures/strategies/ChaseStrategy-test.js
+++ test/client/entities/creatures/strategies/ChaseStrategy-test.js
@@ -1,6 +1,9 @@
 import { default as Dungeon } from '../../../../../src/client/js/app/dungeons/Dungeon.js';
 import { default as PlayableCharacter } from '../../../../../src/client/js/app/entities/creatures/PlayableCharacter.js';
 import { default as SlingshotImp } from '../../../../../src/client/js/app/entities/creatures/enemies/SlingshotImp.js';
+import { default as Ent } from '../../../../../src/client/js/app/entities/creatures/enemies/Ent.js';
+import { default as Tiles } from '../../../../../src/client/js/app/tiles/Tiles.js';
+import { default as Moves } from '../../../../../src/client/js/app/entities/creatures/moves/Moves.js';
 
 const expect = require('chai').expect;
 
@@ -27,5 +30,28 @@ describe('ChaseStrategy', function() {
                 expect(reason).to.be.null;
             }
         }
+    });
+
+    it('should cause a creature to move toward a tile it has seen the player occupy', function() {
+        const dungeon = new Dungeon(3, 3);
+        dungeon.setTile(new Tiles.WallTile(1, 1), 1, 1);
+        dungeon.setTile(new Tiles.WallTile(1, 2), 1, 2);
+        var player = new PlayableCharacter();
+        var enemy = new Ent();
+        dungeon.moveCreature(player, 0, 0);
+        dungeon.moveCreature(enemy, 0, 2);
+        var enemyStartingPosition = dungeon.getTile(enemy);
+
+        expect(enemy.canSee(dungeon, dungeon.getTile(player))).to.be.true;
+
+        player.setNextMove(new Moves.MovementMove(dungeon.getTile(player), 1, 0));
+        player.setNextMove(new Moves.MovementMove(dungeon.getTile(player), 1, 0));
+        player.setNextMove(new Moves.MovementMove(dungeon.getTile(player), 0, 1));
+        player.setNextMove(new Moves.MovementMove(dungeon.getTile(player), 0, 1));
+
+        dungeon.resolveUntilBlocked();
+
+        var enemyPosition  = dungeon.getTile(enemy);
+        expect(enemyPosition).not.to.equal(enemyStartingPosition);
     });
 });

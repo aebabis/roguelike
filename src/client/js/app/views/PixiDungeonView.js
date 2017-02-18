3ae4b50487c3464e2a6b0564eee86536fda4abaa
Alex Babis <acbabis@gmail.com>
2017-02-18 01:53:03 -0700

    Door sprites update again

--- src/client/js/app/views/PixiDungeonView.js
+++ src/client/js/app/views/PixiDungeonView.js
@@ -218,15 +218,23 @@ export default class PixiDungeonView {
 
         sharedData.addObserver((event) => {
             const dungeon = sharedData.getDungeon();
-            if(event instanceof GameEvents.PositionChangeEvent || event instanceof GameEvents.SpawnEvent) {
+            if(event instanceof GameEvents.PositionChangeEvent) {
                 this.updateVision();
                 this.updateCreatureLocations();
+                [event.getFromCoords(), event.getToCoords()].forEach(({x, y}) =>
+                    this._tileContainers[x][y].update()
+                );
+            } else if(event instanceof GameEvents.SpawnEvent) {
+                this.updateCreatureLocations();
+                this._tileContainers[event.getX()][event.getY()].update();
             } else if(event instanceof GameEvents.DeathEvent) {
                 const creature = event.getCreature();
                 const tile = dungeon.getTile(creature);
                 const x = tile.getX();
                 const y = tile.getY();
-                const tileContainer = this._tileContainers[x][y].children[2].removeChildAt(0);
+                const tileContainer = this._tileContainers[x][y];
+                tileContainer.children[2].removeChildAt(0);
+                tileContainer.update();
                 //setTimeout(updateItems); // TODO: Fix with item spawn/drop event
             } else if(event instanceof GameEvents.HitpointsEvent) {
                 /*if(event.getAmount() < 0) {

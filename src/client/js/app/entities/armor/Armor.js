67651cc801411d5e7d350a09ef0cbe3f7382ef75
Alex Babis <acbabis@gmail.com>
2017-08-18 00:57:49 -0700

    Armor tests

--- src/client/js/app/entities/armor/Armor.js
+++ src/client/js/app/entities/armor/Armor.js
@@ -2,18 +2,16 @@ import Item from '../Item.js';
 
 export default class Armor extends Item {
     /**
-      * @class Armor
-      * @description Base class for armor. Can be worn by player or enemy
+      * Base class for armor. Can be worn by player or enemy
       */
     isEquipable() {
         return true;
     }
 
-    getPhysicalReduction() {
-        throw new Error('Abstract method not implemented');
-    }
-
-    getMagicalReduction() {
+    /**
+     * Gets the damage reduction the armor provides against a given damage type
+     */
+    getReduction(type) {
         throw new Error('Abstract method not implemented');
     }
 }
